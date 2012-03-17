12a76eeb2718b09afb730be6977ba6e3348590a1
Emil Karlson <jekarlson@gmail.com>
2012-03-17 16:54:57 +0200

    Add pameter parsing code.

--- fdo-trash.hs
+++ fdo-trash.hs
@@ -1,12 +1,12 @@
 import System.Environment(getArgs,getProgName)
-import System.Console.GetOpt(getOpt,ArgOrder(..),OptDescr(..),ArgDescr(..))
+import System.Console.GetOpt(getOpt,ArgOrder(..),OptDescr(..),ArgDescr(..),usageInfo)
 import System.FilePath.Posix((</>),(<.>))
 import System.Directory(getDirectoryContents)
 import Data.Maybe(catMaybes)
 import Data.Time(getCurrentTimeZone,getCurrentTime,utcToLocalTime)
-import Data.List(sort)
+import Data.List(sort,intercalate)
 import Freedesktop.Trash(TrashFile(..),genTrashFile,trashGetOrphans,getTrashPaths,formatTrashDate,encodeTrashPath)
-
+import Control.Monad(when)
 
 actions =
     [ ("purge", fdoPurge)
@@ -14,6 +14,14 @@ actions =
     , ("unrm", fdoUnRm)
     ]
 
+--compilerOpts :: [String] -> IO (Options, [String])
+parseOpts defaultOptions options exe argv =
+    case getOpt Permute options argv of
+        (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+        (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    where header = "Usage: " ++ exe ++ " [OPTION...] parameters..."
+
+--Rm
 rmFile realPath trashFile = do
     timeZone <- getCurrentTimeZone
     putStrLn "rename:"
@@ -21,19 +29,32 @@ rmFile realPath trashFile = do
     print trashFile
     print $ formatTrashDate (utcToLocalTime timeZone $ deleteTime trashFile)
 
-data RmFlag = Version | Time Int
-    deriving(Show)
+data RmOptions = RmOptions
+    { rmTimeOffset :: Double
+    , rmVersion    :: Bool
+    , rmHelp       :: Bool
+    } deriving(Show)
+
+rmDefaults = RmOptions
+    { rmTimeOffset = 0
+    , rmVersion = False
+    , rmHelp = False
+    }
 
 rmOptions =
-    [ Option ['V'] ["version"] (NoArg Version) "show version number"
-    , Option ['t'] ["time"] (ReqArg (Time . read) "blah") "Specify time offset"
+    [ Option ['V'] ["version"] (NoArg (\opts -> opts{rmVersion=True})) "Show version number"
+    , Option ['h'] ["help"] (NoArg (\opts -> opts{rmHelp=True})) "Print help"
+    , Option ['t'] ["time"] (ReqArg  (\secs opts -> opts{rmTimeOffset=read secs}) "secs")
+        ("Specify time offset, default: " ++ (show $ rmTimeOffset rmDefaults))
     ]
 
 fdoRm args = do
+    (myOpts, realArgs) <- parseOpts rmDefaults rmOptions "fdo-rm" args
+    print myOpts
+    when (null realArgs) $ ioError (userError "No files specified")
+
     now <- getCurrentTime
-    let (opts, realArgs, _) = getOpt Permute rmOptions args
-        time = now
-    print opts
+    let time = now
     (iPath,fPath) <- getTrashPaths
     let file = TrashFile
             (iPath </> head args)
@@ -43,7 +64,38 @@ fdoRm args = do
             0
     rmFile (head realArgs) file
 
+--Purge
+data PurgeOptions = PurgeOptions
+    { purgeThreshold :: Double
+    , purgeAgePow    :: Double
+    , purgeSizePow   :: Double
+    , purgeVersion   :: Bool
+    , purgeHelp      :: Bool
+    } deriving(Show)
+
+purgeDefaults = PurgeOptions
+    { purgeThreshold = 10**6
+    , purgeAgePow = 1
+    , purgeSizePow = 0.1
+    , purgeHelp = False
+    , purgeVersion = False
+    }
+
+purgeOptions =
+    [ Option ['V'] ["version"] (NoArg (\opts -> opts{purgeVersion=True})) "Show version number"
+    , Option ['h'] ["help"] (NoArg (\opts -> opts{purgeHelp=True})) "Print help"
+    , Option ['a'] ["age"] (ReqArg  (\secs opts -> opts{purgeThreshold=read secs}) "secs")
+        ("Specify maximium file age default: " ++ (show $ purgeThreshold purgeDefaults))
+    , Option ['A'] ["age-power"] (ReqArg (\pow opts -> opts{purgeAgePow=read pow}) "pow")
+        ("Specify age power for threshold formula size^sizepow*age^agepow, default: " ++
+        (show $ purgeAgePow purgeDefaults))
+    , Option ['S'] ["size-power"] (ReqArg (\pow opts -> opts{purgeSizePow=read pow}) "pow")
+        ("Specify size power for threshold formula size^sizepow*age^agepow, default: "
+        ++ (show $ purgeSizePow purgeDefaults))
+    ]
+
 fdoPurge args = do
+    (myOpts, _) <- parseOpts purgeDefaults purgeOptions "fdo-purge" args
     (iPath,fPath) <- getTrashPaths
     timeZone <- getCurrentTimeZone
     infoFiles <- fmap (sort.filter (\x -> x /= ".." && x /= ".")) $ getDirectoryContents iPath
@@ -51,17 +103,52 @@ fdoPurge args = do
     let (iExtra,dExtra) = trashGetOrphans infoFiles (sort $ map (<.>"trashinfo") dataFiles)
     print (iExtra,dExtra)
     ayx <- fmap catMaybes $ mapM (genTrashFile iPath fPath timeZone) dataFiles
+    print myOpts
     print args
     print ayx
 
+--Unrm
+data UnRmOptions = UnRmOptions
+    { unRmOrigDir  :: Bool
+    , unRmVersion  :: Bool
+    , unRmHelp     :: Bool
+    , unRmOutFile  :: Maybe String
+    , unRmSelect   :: Maybe String
+    } deriving(Show)
+
+unRmDefaults = UnRmOptions
+    { unRmOrigDir = False
+    , unRmHelp    = False
+    , unRmVersion = False
+    , unRmOutFile = Nothing
+    , unRmSelect  = Nothing
+    }
+
+unRmOptions =
+    [ Option ['V'] ["version"] (NoArg (\opts -> opts{unRmVersion=True})) "Show version number"
+    , Option ['h'] ["help"] (NoArg (\opts -> opts{unRmHelp=True})) "Print help"
+    , Option ['O'] ["original-name"] (NoArg  (\opts -> opts{unRmOrigDir=True}))
+        "output file to original path, default: ., conflicts with -o"
+    , Option ['o'] ["output-file"] (ReqArg (\out opts -> opts{unRmOutFile=Just out}) "filepath")
+        "Specify output file, conflicts with -O"
+    , Option ['s'] ["select"] (ReqArg (\index opts -> opts{unRmSelect=Just $ read index}) "index")
+        "Select file with index if multiple files match"
+    ]
+
+
 fdoUnRm args = do
+    (myOpts, realArgs) <- parseOpts unRmDefaults unRmOptions "fdo-unrm" args
+    print myOpts
+    print realArgs
     putStrLn "TODO"
 
+--Main
 main :: IO ()
 main = do
     args <- getArgs
     exe <- getProgName
-    let thisAction = maybe
+    let actionsStr = intercalate "|" $ map fst actions
+        thisAction = maybe
             ( if (null args)
                 then Nothing
                 else maybe
@@ -72,7 +159,7 @@ main = do
             (lookup (drop 4 exe) actions)
 
     maybe
-        (putStrLn "No action specified")
+        (putStrLn $ "No action specified\nUsage: " ++ exe ++ " <" ++ actionsStr ++ "> params")
         (\(a,f) -> f a)
         thisAction
 
